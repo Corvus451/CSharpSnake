86f71a5a3068df8a0dfdfb18219809e08bef66c6
Corvus <msmark.451@gmail.com>
2025-10-12 19:47:42 +0200

    Created Position2d class.

---
+++ SnakeGame/model/Position2d.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.model
+{
+    public record Position2d(int X, int Y)
+    {
+        public static Position2d operator +(Position2d a, Position2d b)
+        {
+            return new Position2d(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Position2d operator -(Position2d a, Position2d b)
+        {
+            return new Position2d(a.X - b.X, a.Y - b.Y);
+        }
+    }
+}
