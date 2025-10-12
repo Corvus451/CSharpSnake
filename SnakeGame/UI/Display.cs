0cf52eb3a2075f65f340ee9212a4eca108c1c357
Corvus <msmark.451@gmail.com>
2025-10-12 19:33:24 +0200

    Created Display class

---
+++ SnakeGame/UI/Display.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.UI
+{
+    public static class Display
+    {
+
+        public static void ResetWindow(int width, int heigth)
+        {
+            if (Console.WindowWidth != width || Console.WindowHeight != heigth)
+            {
+                Console.SetWindowSize(width, heigth);
+                Console.SetBufferSize(width, heigth);
+                Console.CursorVisible = false;
+            }
+        }
+
+        public static void Draw(int x, int y, char ch)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ch);
+        }
+
+        public static void Clear(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+        }
+    }
+}
