23588a5bc5c67064da6de2dd1ff56303703905b5
Corvus <msmark.451@gmail.com>
2025-10-12 20:09:23 +0200

    Created Player class.

---
+++ SnakeGame/model/actors/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.model.actors
+{
+    class Player : Actor
+    {
+        public bool Alive;
+
+        public Player(Position2d position, Board board) : base(position, '@', board)
+        {
+            Alive = true;
+        }
+
+        public void End()
+        {
+            Alive = false;
+        }
+    }
+}
